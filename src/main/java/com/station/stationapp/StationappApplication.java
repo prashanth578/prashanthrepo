17c265202363b90a67065a521692ab9fc5be4bf0
Prashanth <prashanthworkmail@gmail.com>
2018-12-17 20:36:03 -0500

    Pushing App to Remote Repository

---
+++ src/main/java/com/station/stationapp/StationappApplication.java
@@ -0,0 +1,14 @@
+package com.station.stationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationappApplication.class, args);
+	}
+
+}
+
