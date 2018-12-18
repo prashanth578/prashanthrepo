417707625915669c493d8d03a75a84d5e17d026c
Prashanth <prashanthworkmail@gmail.com>
2018-12-17 23:21:24 -0500

    Adding Dependencies fix , Swagger Config

---
+++ src/main/java/com/station/stationapp/resource/VehicleNotFoundException.java
@@ -0,0 +1,10 @@
+package com.station.stationapp.resource;
+
+
+public class VehicleNotFoundException extends RuntimeException {
+
+	public VehicleNotFoundException(String exception) {
+		super(exception);
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+insert into Vehicle
+values(1,'Toyota', 'Sedan','8','blue','2015','FourCylinder', 'YES','A2Z134','Iron');
+
+insert into Vehicle
+values(2,'Benz', 'Hatch Back','4','Red','2018','SixCylinder', 'YES','GAR358','Steel');
+
+insert into Vehicle
+values(3,'Hundai', 'Coupe','4','Black','2017','SixCylinder', 'YES','CXF117','Aluminium');
+
+insert into Vehicle
+values(4,'Honda', 'Truck','4','Silver','2018','FourCylinder', 'YES','AMF121','Aluminium');
+
+insert into Vehicle
+values(5,'Audi', 'Sedan','4','Gray','2019','EightCylinder', 'YES','AXF181','Iron');
